b203741f17db417089403bc9baa7123ede4378e4
scatmanspace <julianilgf@gmail.com>
2023-04-07 19:31:23 -0400

    Implemented double quote enclosed commas

--- src/driver/Main.java
+++ src/driver/Main.java
@@ -13,14 +13,30 @@ public class Main {
     public static PrintWriter out;
     public static Scanner in;
 
+    /**
+     * The convert method is the one that handles converting CSV to TXT and vice versa. It takes in the input and output
+     * formats, then for each line in the input file it splits the rows into cells using the delimiter of the input format
+     * (for csv files it splits using regex which accounts for commas enclosed in double quotations).
+     * and then rejoins the cells with the output format/s delimiter and writes it in the output file.
+     *
+     * @param inFormat  is the format of the input file.
+     * @param outFormat is the format of the output file.
+     */
     public static void convert(String inFormat, String outFormat) {
         while (in.hasNextLine()) {
             String line = in.nextLine(); // splits file into lines
-            String[] cells = line.split(inFormat.equalsIgnoreCase("txt") ? "\t" : ","); // splits lines into cells using input separator
+            String[] cells = line.split(inFormat.equalsIgnoreCase("txt") ? "\t" : ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"); // splits lines into cells using input separator accounting for double quotation enclosed commas
             out.println(String.join(outFormat.equalsIgnoreCase("txt") ? "\t" : ",", cells)); // joins cells with output separator
         }
     }
 
+    /**
+     * The readFile method takes in a file input stream and opens the file and stores the rows as strings in an ArrayList,
+     * then it returns the Arraylist.
+     *
+     * @param inStream is the file input stream.
+     * @return returns the ArrayList of row strings.
+     */
     public static ArrayList<String> readFile(FileInputStream inStream) {
         Scanner inFile = new Scanner(inStream);
         ArrayList<String> rv = new ArrayList<>();
@@ -30,6 +46,14 @@ public class Main {
         return rv;
     }
 
+    /**
+     * The writeFile stream takes in two parameters, an ArrayList of rows and a file output stream. The print writer is
+     * initialized with the file stream and then each item from the arraylist of rows is written to the file. After its
+     * done it closes the file.
+     *
+     * @param rows      is the ArrayList of rows in string format.
+     * @param outStream is the File output stream.
+     */
     public static void writeFile(ArrayList<String> rows, FileOutputStream outStream) {
         PrintWriter outFile = new PrintWriter(outStream);
         for (String row : rows)
@@ -38,78 +62,70 @@ public class Main {
     }
 
     public static void main(String[] args) {
+
         Scanner keyboard = new Scanner(System.in);
 
         while (true) {
 
             System.out.println("Enter Command: ");
-            String command = keyboard.nextLine();
+            String command = keyboard.nextLine();   // scans for commands
 
             try {
 
                 if (command.equals("quit"))
                     break;
-                else if (command.equals("close")) {
+                else if (command.startsWith("convert")) {
+
+                    String[] tokens = command.split(" "); // splits command into argument tokens
+
+                    if (tokens.length != 3) // verifies there is the correct number of tokens
+                        throw new Exception("Illegal convert command. Usage: command fileName1 fileName2");
+
+                    String inputFormat = tokens[1].substring(tokens[1].length() - 3);  // extracts the file extension from the file name
+                    String outputFormat = tokens[2].substring(tokens[2].length() - 3); // extracts the file extension from the file name
+
+                    if (!inputFormat.equalsIgnoreCase("csv") && !inputFormat.equalsIgnoreCase("txt")) // verifies file formats
+                        throw new Exception("Illegal input format! Supported formats: txt and csv");
+
+                    if (!outputFormat.equalsIgnoreCase("csv") && !outputFormat.equalsIgnoreCase("txt")) // verifies file formats
+                        throw new Exception("Illegal output format! Supported formats: txt and csv");
+
+                    if (tokens[1].equalsIgnoreCase(tokens[2])) // makes sure input and output files are not the same
+                        throw new Exception("Source and destination files are the same for the convert command");
+
+                    in = new Scanner(new FileInputStream(INPUT_PATH + tokens[1])); // creates input scanner
+
+                    out = new PrintWriter(new FileOutputStream(OUTPUT_PATH + tokens[2])); // creates output printWriter
+
+                    convert(inputFormat, outputFormat); // converts the file
+
+                    // closes the files.
                     in.close();
                     out.close();
 
-                } else if (command.startsWith("convert")) {
-
-                    String[] tokens = command.split(" ");
-
-                    if (tokens.length != 3)
-                        throw new Exception("Illegal convert command. Usage: command fileName1 fileName2");
-
-                    String inputFormat = tokens[1].substring(tokens[1].length() - 3);
-                    String outputFormat = tokens[2].substring(tokens[2].length() - 3);
-
-                    if (!inputFormat.equalsIgnoreCase("csv") && !inputFormat.equalsIgnoreCase("txt"))
-                        throw new Exception("Illegal input format! Supported formats: txt and csv");
-
-                    if (!outputFormat.equalsIgnoreCase("csv") && !outputFormat.equalsIgnoreCase("txt"))
-                        throw new Exception("Illegal output format! Supported formats: txt and csv");
-
-                    if (tokens[1].equalsIgnoreCase(tokens[2]))
-                        throw new Exception("Source and destination files are the same for the convert command");
-
-                    in = new Scanner(new FileInputStream(INPUT_PATH + tokens[1]));
-
-                    out = new PrintWriter(new FileOutputStream(OUTPUT_PATH + tokens[2]));
-
-                    convert(inputFormat, outputFormat);
-
                 } else if (command.startsWith("normalize")) {
 
-                    String[] tokens = command.split(" ");
+                    String[] tokens = command.split(" "); // splits command into argument tokens
 
-                    if (tokens.length != 2)
+                    if (tokens.length != 2) // verifies there is the correct number of tokens
                         throw new Exception("Illegal normalize command. Usage: normalize fileName");
 
                     String inputFormat = tokens[1].substring(tokens[1].length() - 3);
 
-                    if (!inputFormat.equalsIgnoreCase("csv") && !inputFormat.equalsIgnoreCase("txt"))
+                    if (!inputFormat.equalsIgnoreCase("csv") && !inputFormat.equalsIgnoreCase("txt")) // verifies file formats
                         throw new Exception("Illegal input format! Supported formats: txt and csv");
 
-                    ArrayList<String> rows = readFile(new FileInputStream(INPUT_PATH + tokens[1]));
+                    ArrayList<String> rows = readFile(new FileInputStream(INPUT_PATH + tokens[1])); // stores the file in an arraylist of rows
 
-                    ArrayList<String> normalizedRows = Normalizer.normalizeTable(rows, inputFormat);
+                    ArrayList<String> normalizedRows = Normalizer.normalizeTable(rows, inputFormat); // normalizes and stores normalized rows in and arraylist of rows
 
-                    writeFile(normalizedRows, new FileOutputStream(INPUT_PATH + tokens[1]));
+                    writeFile(normalizedRows, new FileOutputStream(INPUT_PATH + tokens[1])); // overwrites the normalized rows arraylist to the file
 
                 } else
                     throw new Exception("Illegal command is entered!");
 
             } catch (Exception exp) {
-
                 System.out.println("Something went wrong: " + exp.getMessage() + "\nTry again!");
-
-            } finally {
-                if (in != null) {
-                    in.close();
-                }
-                if (out != null) {
-                    out.close();
-                }
             }
         }
     }
--- src/util/Normalizer.java
+++ src/util/Normalizer.java
@@ -3,31 +3,39 @@ package util;
 import java.util.*;
 
 public class Normalizer {
+
+    /**
+     * Normalize is a method which normalizes a given cell and formats it according to the type of content in it.
+     *
+     * @param cell is the cell to be normalized and then returned formatted.
+     * @return returns the formatted/normalized cell
+     */
     private static String normalize(String cell) {
         if (cell.isEmpty())
             return "N/A";
+
         try {
-            int ival = Integer.parseInt(cell); // parses the cell as int and throws exception if not the correct format
+            int intVal = Integer.parseInt(cell); // parses the cell as int and throws exception if not the correct format
 
-            String rv = "" + Math.abs(ival);   // stores the absolute value with no zeros
+            StringBuilder rv = new StringBuilder(String.valueOf(Math.abs(intVal)));   // stores the absolute value with no zeros
 
-            for (int i = 0; i < 9 - ("" + Math.abs(ival)).length(); i++)
-                rv = "0" + rv; // adds a zero until length is 10
+            for (int i = 0; i < 9 - (String.valueOf(Math.abs(intVal))).length(); i++)
+                rv.insert(0, "0"); // adds a zero until length is 9
 
-            rv = (ival >= 0 ? "+" : "-") + rv; // adds a + if the initial value was positive and - if it was negative
+            rv.insert(0, (intVal >= 0 ? "+" : "-")); // adds + if the initial value was positive and - if it was negative
 
-            return rv;
+            return rv.toString();
 
         } catch (NumberFormatException exp) {
 
             try {
                 //non-integer
-                double dval = Double.parseDouble(cell); // parses the cell as double and throws exception if not the correct format
+                double doubleVal = Double.parseDouble(cell); // parses the cell as double and throws exception if not the correct format
 
-                if (dval > 100 || dval < 0.01)
-                    return String.format("%.2e", dval); // if the double value is greater than 100 or less than 0.01 then it returns in exponential form with two decimal precision
+                if (doubleVal > 100 || doubleVal < 0.01)
+                    return String.format("%.2e", doubleVal); // if the double value is greater than 100 or less than 0.01 then it returns in exponential form with two decimal precision
 
-                return String.format("%.2f", dval); // else it returns the value in double format with two decimal precision
+                return String.format("%.2f", doubleVal); // else it returns the value in double format with two decimal precision
 
             } catch (NumberFormatException exp2) {
 
@@ -41,28 +49,34 @@ public class Normalizer {
         }
     }
 
+    /**
+     * normalizeTable is the method that is called to normalize/format entire files. It is given two parameters, an
+     * ArrayList of rows and a file format. First, it breaks down the rows into cells using the given file formats
+     * delimiter, then it normalizes each cell in that row, then it merges the now normalized cells with the same
+     * delimiter, and then it repeats for every other row.
+     *
+     * @param rows       is the ArrayList of rows from the target document.
+     * @param fileFormat is the file type of the target file.
+     * @return returns an ArrayList of normalized rows to be written back into the target file.
+     */
     public static ArrayList<String> normalizeTable(ArrayList<String> rows, String fileFormat) {
         //Step 1: Break down a row into cells
         //Step 2: Call normalize to normalize each cell
         //Step 3: Merge normalized cells with the same separator that you used to break them down.
         //Step 4: Repeat for all rows.
 
-        String separator = fileFormat.equalsIgnoreCase("txt") ? "\t" : ",";
-
         String[] cells;
-
-        ArrayList<String> normalizedCells = new ArrayList<>();
         ArrayList<String> normalizedRows = new ArrayList<>();
+        String splitSeparator = fileFormat.equalsIgnoreCase("txt") ? "\t" : ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"; // accounts for cells with double quotation enclosed commas
+        String joinSeparator = fileFormat.equalsIgnoreCase("txt") ? "\t" : ",";
 
         for (String row : rows) {
-            cells = row.split(separator); // splits row into cells using file format separator
+            cells = row.split(splitSeparator); // splits row into cells using file format split separator
 
-            for (String cell : cells) {
-                normalizedCells.add(normalize(cell)); // normalizes the cells and adds them to normalizedCells
-            }
+            for (int i = 0; i < cells.length; i++)
+                cells[i] = normalize(cells[i]); // overwrites the cells with the normalized version
 
-            normalizedRows.add(String.join(separator, normalizedCells)); // joins normalizedCells using the file format separator
-            normalizedCells.clear(); // clears normalized Cells array list in preparation for next row/
+            normalizedRows.add(String.join(joinSeparator, cells)); // joins normalizedCells using the file format join separator and adds them to the rows ArrayList
         }
         return normalizedRows; // returns the ArrayList of row strings
     }
